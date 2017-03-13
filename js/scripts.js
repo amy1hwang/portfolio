5bead10eaa671a8c3f0d5fded87f2f102112ea58
Amy Hwang <amyhwang1218@gmail.com>
2017-03-12 19:16:08 -0700

    add media query stylings

--- js/scripts.js
+++ js/scripts.js
@@ -1,50 +1,58 @@
-//sticky bar
-var num = 100;
-$(window).bind('scroll', function () {
-    if ($(window).scrollTop() > num) {
-      $('nav').addClass('fixed');
-    } else {
-      $('nav').removeClass('fixed');
-    };
-});
-
-//scroll to the next section
 $(function() {
+
+  //sticky bar
+  var navTop = $('nav').offset().top;
+  var aboutTop = $('.about').offset().top - 40;
+  var projectsTop = $('.projects').offset().top;
+  $(window).bind('scroll', function () {
+      if ( aboutTop < $(window).scrollTop() && $(window).scrollTop() < projectsTop) {
+        $('nav').addClass('reverse');
+      } else if ( navTop < $(window).scrollTop()) {
+        $('nav').removeClass('reverse');
+        $('nav').addClass('fixed');
+      } else {
+        $('nav').removeClass('reverse');
+        $('nav').removeClass('fixed');
+      };
+  });
+
+
+  //scroll to the next section
   $('.to-about').click(function() {
     $('body,html').animate({
-      scrollTop: $("#about").offset().top},
-      300);
+      scrollTop: $("#about").offset().top + 40},
+      500);
   });
   $('.to-projects').click(function() {
     $('body,html').animate({
       scrollTop: $("#projects").offset().top},
-      300);
+      500);
   });
   $('.to-contact').click(function() {
     $('body,html').animate({
       scrollTop: $("#contact").offset().top},
-      300);
+      500);
   });
-});
 
 
 
-//show to-the-top-icon
-var n = 250;
-$(window).scroll(function() {
-  if ($(this).scrollTop() > n) {
-    $('.to-the-top').fadeIn();
-  } else {
-    $('.to-the-top').fadeOut();
-  }
-});
 
-//scroll up to the top
-$(function() {
+  //show to-the-top-icon
+  var n = 250;
+  $(window).scroll(function() {
+    if ($(this).scrollTop() > n) {
+      $('.to-the-top').fadeIn();
+    } else {
+      $('.to-the-top').fadeOut();
+    }
+  });
+
+  //scroll up to the top
+
   $('.to-the-top').click(function() {
-    $('body,html').animate({
+    $('html, body').animate({
       scrollTop : 0
-    }, 800);
+    }, 500);
     $('.to-the-top').css('display', 'none');
   });
 })
